597b1a73a0331c32416daef21d01ad64511ffe35
Chris Hudson <chris@xetius.com>
2015-03-01 22:20:12 +0000

    Finalised tidying

--- src/main/java/com/xetius/gs/Main.java
+++ src/main/java/com/xetius/gs/Main.java
@@ -1,17 +1,21 @@
 package com.xetius.gs;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.List;
+
+import static java.lang.Math.min;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         final CharacterListWordReader reader = new CharacterListWordReader();
         final WordTreeStorage storage = new WordTreeStorage();
-
-        if (reader.open(new FileInputStream(args[0]))) {
+        final InputStream stream = getStreamFromFile(args[0]);
+        if (reader.open(stream)) {
 
             List<Character> word;
             while (!reader.isEndOfFile()) {
@@ -24,12 +28,20 @@ public class Main {
             final List<WordCountItem> counts = storage.getCounts();
             Collections.sort(counts);
 
-            for (WordCountItem item : counts.subList(0, 20)) {
+            for (WordCountItem item : counts.subList(0, min(20, counts.size()))) {
                 System.out.println(item);
             }
 
         } else {
-            System.out.println("Bugger");
+            System.out.println("Unable to open file " + args[0]);
+        }
+    }
+
+    private static InputStream getStreamFromFile(String fileName) {
+        try {
+            return new FileInputStream(fileName);
+        } catch (FileNotFoundException e) {
+            return null;
         }
     }
 }
